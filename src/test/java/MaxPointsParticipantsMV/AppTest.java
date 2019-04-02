39c333de1884e9bd131b456c8c3d3202d99aa539
Noixelfer <pauldarius98@gmail.com>
2019-04-03 00:18:32 +0300

    Add more tests in AppTest, now students and teme paths are saved in Strings

--- src/test/java/MaxPointsParticipantsMV/AppTest.java
+++ src/test/java/MaxPointsParticipantsMV/AppTest.java
@@ -2,19 +2,22 @@ package MaxPointsParticipantsMV;
 
 import Domain.Student;
 import Repository.StudentRepo;
+import Service.ServiceStudent;
 import Validator.StudentValidator;
+import org.junit.Assert;
 import org.testng.annotations.Test;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest
 {
+    private static String studentsPath = "src\\studenti.xml";
+    private static String temePath = "src\\teme.xml";
     /**
      * Rigorous Test :-)
      */
@@ -22,10 +25,97 @@ public class AppTest
     @Test
     public void addStudentSuccessfully()
     {
-        StudentRepo rep = new StudentRepo(new StudentValidator(), "src\\\\studenti.xml");
+        StudentRepo rep = new StudentRepo(new StudentValidator(), studentsPath);
         Student paul = new Student("10", "Paul", 936, "paul.pop@yahoo.com", "Profesor X");
         float size = rep.size();
         rep.save(paul);
         assert(rep.size() == size+ 1);
+    }
+
+    @Test
+    public void addStudentWithInvalidEmail() {
+        try {
+            StudentRepo rep = new StudentRepo(new StudentValidator(), studentsPath);
+            ServiceStudent srv = new ServiceStudent(rep);
+            Student std = new Student("78", "Sergiu", 936, "e-mail", "Prof X");
+            srv.add(std);
+            assertTrue(false);
+        } catch (Exception e) {
+            assertTrue(true);
+        }    }
+
+    @Test
+    public void addStudentWithNullEmail() {
+        try {
+            StudentRepo rep = new StudentRepo(new StudentValidator(), studentsPath);
+            ServiceStudent srv = new ServiceStudent(rep);
+            Student std = new Student("78", "Sergiu", 936, null, "Prof X");
+            srv.add(std);
+            Assert.assertTrue(false);
+        } catch (Exception e) {
+            assertTrue(true);
+        }       }
+
+    @Test
+    public void addStudentWithInvalidGroup() {
+        try {
+            StudentRepo rep = new StudentRepo(new StudentValidator(), studentsPath);
+            ServiceStudent srv = new ServiceStudent(rep);
+            Student std = new Student("78", "Sergiu", 189, "jojo@ghdd.com", "Prof X");
+            srv.add(std);
+            assertTrue(false);
+        } catch (Exception e) {
+            assertTrue(true);
+        }      }
+
+    @Test
+    public void addStudentWithNullName() {
+        try {
+            StudentRepo rep = new StudentRepo(new StudentValidator(), studentsPath);
+            ServiceStudent srv = new ServiceStudent(rep);
+            Student std = new Student("78", null, 933, "jojo@ghdd.com", "Prof X");
+            srv.add(std);
+            assertTrue(false);
+        } catch (Exception e) {
+            assertTrue(true);
+        }        }
+
+    @Test
+    public void addStudentWithNullProfessor() {
+        try {
+            StudentRepo rep = new StudentRepo(new StudentValidator(), studentsPath);
+            ServiceStudent srv = new ServiceStudent(rep);
+            Student std = new Student("78", "Sergiu", 933, "jojo@ghdd.com", null);
+            srv.add(std);
+            assertTrue(false);
+        } catch (Exception e) {
+            assertTrue(true);
+        }
+    }
+
+    @Test
+    public void addStudentWithInvalidProfessor() {
+        try {
+            StudentRepo rep = new StudentRepo(new StudentValidator(), studentsPath);
+            ServiceStudent srv = new ServiceStudent(rep);
+            Student std = new Student("78", "Sergiu", 935, "jojo@ghdd.com", "Prof 66X");
+            srv.add(std);
+            assertTrue(false);
+        } catch (Exception e) {
+            assertTrue(true);
+        }
+    }
+
+    @Test
+    public void addStudentWithInvalidID() {
+        try {
+            StudentRepo rep = new StudentRepo(new StudentValidator(), studentsPath);
+            ServiceStudent srv = new ServiceStudent(rep);
+            Student std = new Student("7j8", "Sergiu", 934, "jojo@ghdd.com", "Prof X");
+            srv.add(std);
+            assertTrue(false);
+        } catch (Exception e) {
+            assertTrue(true);
+        }
     }
 }
